4d40440483af05194d4ff6a58871fac5b4d3d3ae
Ken Sodemann <kenneth.sodemann@outsystems.com>
2025-02-13 14:00:41 -0600

    fix(): do not overlay the web view with the status bar

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,12 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.acconfigvue',
   appName: 'AC Config Tool',
   webDir: 'dist',
-  bundledWebRuntime: false,
   plugins: {
     SplashScreen: {
       launchAutoHide: false,
+    },
+    StatusBar: {
+      overlaysWebView: false,
     },
   },
 };
